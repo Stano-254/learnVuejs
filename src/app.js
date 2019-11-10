3e7961210c8d1ddb0bc97244c338f667f601e4d0
stano-254 <stanoemali87@gmail.com>
2019-11-10 18:54:22 +0300

    start up

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+new Vue({
+    el:'#app',
+    data: {
+        title : 'vue js seem fun',
+        content:'way more fun to code this ',
+        menu:'my menu data here'
+    }
+});
