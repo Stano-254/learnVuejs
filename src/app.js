b8a838cc17de8f55cd228bfbe7e63f46f6ef54e2
stano-254 <stanoemali87@gmail.com>
2019-11-11 00:20:25 +0300

    use of Computed feature

--- src/app.js
+++ src/app.js
@@ -3,12 +3,29 @@ new Vue({
     el:'#app',
     data: {
         title : 'vue js seem fun',
-        // content:'way more fun to code this ',
-        // menu:'my menu data here'
-        items:'',
         items:[
-            'item1',
-            'item2'
-        ],
+            {title:'Title 1',description:'this is description for heading 1'}            {text:'item2',checked:true},
+            {title:'Title 2',description:'this is description for heading 2'}            {text:'item2',checked:true},
+            {title:'Title 3',description:'this is description for heading 3'}            {text:'item2',checked:true},
+            {text:'item3',checked:false},
+
+            ],
+        price:10
+
+    },
+    computed: {
+        tax:function(){
+            return parseFloat(this.price*0.09)
+        } ,
+        total:function(){
+            return parseFloat(this.price) + this.tax
+        },
+
+    },
+    methods:{
+        moneyFormat:function (price) {
+            return '$' + price
+        }
     }
+
 });
